e0b51a19bfcb5c840091255a357b056704862eac
Konstantin Emtsev <k.emtsev.dev@gmail.com>
2025-11-30 18:18:21 +0300

    up

--- src/core/main-loader.js
+++ src/core/main-loader.js
@@ -1,23 +1,14 @@
 import Loadable from "@ips/app/loadable-vjs";
-// const Spinner = ()=><div>Loading....</div>
-// import Spinner from './gooey-spinner-vjs'
 import app from "@ips/app/app";
 import { loadScriptCb } from '@ips/app/utils.js'
 
 import { preloader } from "./preloader";
-import initStrapi from '@ips/strapi/client';
-import tmStrapiData from '@/utils/tmStrapiData'
 import loadJson from '@ips/app/loadJson'
 import loadText from '@ips/app/loadText'
 
 import initFonts from '../fonts'
 
 import content from '../content'
-
-// window.__ips_public_path__ = window.__ips_public_path__ || app.publicPath || '/'
-
-// import * as Critical from './critical'
-// import keen from './utils/keen'
 
 const ensureEndSlash = (s) => (s[s.length - 1] == "/" ? s : s + "/");
 
@@ -38,66 +29,25 @@ export default {
         trace('search locale:', searchLocale)
 
         const loadData = (async()=>{
-
           const cfgName = (__BUILD_MODE__ == "production") ? "config.prod" : "config"
-
           const dataConfig = await loadJson(`${app.publicPath}/${cfgName}.json`)
           const locale = searchLocale || dataConfig.locale || 'ru'
           trace('locale:', locale)
           opts.locale = locale
-
           return content
-          
-          // if(dataConfig.mode == 'local'){
-          //   const result = await loadJson(`${app.publicPath}/data-${locale}.json`)
-          //   // trace(result)
-          //   return result
-          // }else
-          // if(dataConfig.mode == 'strapi'){
-          //   const dataq = await loadText('data.gql')
-          //   const strapi = initStrapi(dataConfig.backURI)
-          //   let result = await strapi.getData(dataq, { locale })
-          //   trace('got raw data', result)
-          //   // result.locale = locale
-          //   result = tmStrapiData(result)
-          //   result.config = dataConfig
-          //   return result
-          // }else{
-          //   error('confg.json error: mode=(local|strapi)')
-          //   return {}
-          // }
-
-          // cb(result)
         })
 
         const dataPromise = loadData()
 
         function onLoadChunk(event) {
-          // console.log("RESOURCES", event.detail.resource.url);
           preloader.setProgress(event.detail.loaded, event.detail.total);
-          //   event.detail.loaded; // total bytes loaded
-          //   event.detail.total; // total bytes requested
-          //   (event.detail.loaded / event.detail.total) * 10 * 100; // total progress percentage
-          //   const resource = event.detail.resource; // info about resource that triggered the event
-          //   resource.loaded;
-          //   resource.total;
-          //   resource.url;
-          //   console.log("EVENT DETAIL", event.detail);
-          //   console.log("EVENT Total", event.detail.total);
         }
 
         document.addEventListener("chunk-progress-webpack-plugin", onLoadChunk);
         // trace('app.publicPath', app.publicPath)
-        // trace('__ips_public_path__', __ips_public_path__)
-        // trace('__webpack_public_path__ 0', __webpack_public_path__)
         __webpack_public_path__ = ensureEndSlash(
           app.publicPath || __webpack_public_path__
         );
-        // keen.set({
-        //     publicPath: app.publicPath,
-        //     __webpack_public_path__
-        // })
-        // keen.log('loader')
 
         initFonts(app.publicPath)
         
